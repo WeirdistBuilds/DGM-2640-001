a728fe0188a2ccce022dc75d72ee795d6445d22a
Unknown <kylehinckley@hotmail.com>
2019-01-10 10:54:56 -0700

    SeaEnvironment Copy

---
+++ UnityProjects/SeaEnvironment/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+
+	private Transform myTransform;
+
+	private void Start()
+	{
+		myTransform = GetComponent<Transform>();
+	}
+
+	private void Update()
+	{
+		myTransform.Rotate(new Vector3(0, 20, 0) * Time.deltaTime);
+	}
+}
