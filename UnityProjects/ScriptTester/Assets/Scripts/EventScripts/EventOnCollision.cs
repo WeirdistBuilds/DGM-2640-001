08ab2219146d9921cef17c34107d521d1bcfac53
Unknown <kylehinckley@hotmail.com>
2019-01-28 23:58:50 -0700

    ScriptTester Project

---
+++ UnityProjects/ScriptTester/Assets/Scripts/EventScripts/EventOnCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnCollision : MonoBehaviour
+{
+	public UnityEvent Event;
+
+	private void OnCollisionEnter()
+	{
+		Event.Invoke();
+	}
+}
---
+++ UnityProjects/ScriptTester/Assets/Scripts/Popper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Popper : MonoBehaviour
+{
+	private Rigidbody[] rigidBodies;
+
+	private void OnCollisionEnter(Collision other)
+	{
+		rigidBodies = gameObject.GetComponentsInChildren<Rigidbody>();
+
+		foreach (var rigidBody in rigidBodies)
+		{
+			rigidBody.isKinematic = false;
+		}
+		
+		Destroy(gameObject, 3);
+	}
+}
