16f26c1415d8057c630820e6881d3d4039a7322f
Unknown <kylehinckley@hotmail.com>
2019-01-31 14:45:11 -0700

    Sea Turtle and Script Project Start

---
+++ UnityProjects/Anthony Scripts/Assets/Scripts/Behaviours.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Behaviours : MonoBehaviour
+{
+
+	public UnityEvent OnAwake, OnStart, TriggerEnter, CollisionEnter;
+	
+	void Awake()
+	{
+		OnAwake.Invoke();
+	}
+	
+	void Start ()
+	{
+		OnStart.Invoke();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		TriggerEnter.Invoke();
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		CollisionEnter.Invoke();
+	}
+
+	private void OnDestroy()
+	{
+		
+	}
+}
