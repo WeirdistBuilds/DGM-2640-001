0ba8003ebcfc23b2d8513788f02e66879b8794b7
Unknown <kylehinckley@hotmail.com>
2019-01-31 19:50:51 -0700

    Camera Controller

---
+++ UnityProjects/ScriptTester/Assets/Scripts/CameraController/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject myPlayer;
+	private Vector3 myPlayerPosition;
+	private Vector3 tempCameraPosition;
+	public float offset, speed;
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		float step = speed * Time.deltaTime;
+		myPlayerPosition = myPlayer.GetComponent<Transform>().position;
+		tempCameraPosition = new Vector3(myPlayerPosition.x, myPlayerPosition.y + offset, myPlayerPosition.z - offset);
+		transform.position = Vector3.MoveTowards(transform.position, tempCameraPosition, step);
+	}
+}
