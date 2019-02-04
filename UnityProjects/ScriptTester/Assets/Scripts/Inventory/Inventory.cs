8daab3b38e9f3b921d2fed2db25278b2b18bd657
Unknown <kylehinckley@hotmail.com>
2019-02-04 01:26:07 -0700

    Inventory Start

---
+++ UnityProjects/ScriptTester/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+	public Image[] itemImages = new Image[NumItemSlots];
+	public ItemSO[] items = new ItemSO[NumItemSlots];
+	
+	public const int NumItemSlots = 4;
+
+	public void AddItem(ItemSO itemToAdd)
+	{
+		for (int i = 0; i < items.Length; i++)
+		{
+			if (items[i] == null)
+			{
+				items[i] = itemToAdd;
+				itemImages[i].sprite = itemToAdd.sprite;
+				itemImages[i].enabled = true;
+				return;
+			}	
+		}
+	}	
+	
+	public void RemoveItem(ItemSO itemToRemove)
+	{
+		for (int i = 0; i < items.Length; i++)
+		{
+			if (items[i] == itemToRemove)
+			{
+				items[i] = null;
+				itemImages[i].sprite = null;
+				itemImages[i].enabled = false;
+				return;
+			}	
+		}
+	}		
+}
---
+++ UnityProjects/ScriptTester/Assets/Scripts/Inventory/ItemSO.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (fileName = "NewItem", menuName = "Scriptable Objects/Inventory/Item")]
+public class ItemSO : ScriptableObject
+{
+    public Sprite sprite;
+}
