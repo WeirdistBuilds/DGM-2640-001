43b3041d95f783788e6bc7df90a7af6c42c80d69
Unknown <kylehinckley@hotmail.com>
2019-02-14 12:56:18 -0700

    Character Controller Script

---
+++ UnityProjects/Anthony Scripts/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+
+	private CharacterController controller;
+	private Vector3 position;
+	public float MoveSpeed = 5;
+	public float Gravity = -9.81f;
+	
+	
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update()
+	{
+		position.x = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
+		if (controller.isGrounded)
+		{
+			if (Input.GetButton("Jump"))
+			{
+				position.y += MoveSpeed;
+			}
+		}
+
+		position.y = Gravity * Time.deltaTime;
+		
+		controller.Move(position);
+	}
+}
