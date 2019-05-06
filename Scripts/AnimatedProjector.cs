211b4af7d61859df08c0698b72e8b5afa92c0982
Unknown <kylehinckley@hotmail.com>
2019-05-06 22:06:19 +0800

    Sine Mover and Animated Projector Added to Scripts

---
+++ Scripts/AnimatedProjector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class AnimatedProjector : MonoBehaviour {
+
+    public float fps = 30.0f;
+    public Texture2D[] frames;
+
+    private int frameIndex;
+    private Projector projector;
+
+    private void Start()
+    {
+        projector = GetComponent<Projector>();
+        NextFrame();
+        InvokeRepeating("NextFrame", 1 / fps, 1 / fps);
+    }
+
+    void NextFrame()
+    {
+        projector.material.SetTexture("_ShadowTex", frames[frameIndex]);
+        if (frameIndex >= frames.Length - 1)
+        {
+            frameIndex = 0;
+        }
+        else
+        {
+            frameIndex++;
+        }
+    }
+}
---
+++ Scripts/SineMover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SineMover : MonoBehaviour
+{
+	private float degrees;
+	public float Amplitude = 1.0f;
+	public float Period = 1.0f;
+
+	void Update()
+	{
+		float degreesPerSecond = 360.0f / Period;
+		degrees = Mathf.Repeat(degrees + Time.deltaTime * degreesPerSecond, 360.0f);
+		float radians = degrees * Mathf.Deg2Rad;
+		transform.localPosition = new Vector3(transform.localPosition.x, Amplitude * Mathf.Sin(radians), transform.localPosition.z);
+	}
+}
